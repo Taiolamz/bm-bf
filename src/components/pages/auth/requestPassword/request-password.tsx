4907aa3719098b1d7714f50dbd0b51d692f0385b
Taiolamz <hassanadefarati@gmail.com>
2023-10-30 16:59:39 +0100

    OTP conversion

--- src/components/pages/auth/requestPassword/request-password.tsx
+++ src/components/pages/auth/requestPassword/request-password.tsx
@@ -3,9 +3,15 @@ import CheckImg from "../../../../assets/check-img.svg";
 import { RevvexButton } from "../../../buttons/button";
 import AuthLayout from "../layout/auth";
 import "./request-password.css";
+import { useState } from "react";
+import ReactPinField from "react-pin-field";
 
 const RequestPassword = () => {
   const navigate = useNavigate();
+
+  const [showActive, setShowActive] = useState(false);
+  const [token, setToken] = useState("");
+  const [error, setError] = useState("");
 
   return (
     <AuthLayout>
@@ -21,12 +27,33 @@ const RequestPassword = () => {
 
         {/* text start */}
         <p>
-          You will receive a system generated password soon, proceed to confirm
-          your new password
+          A One-Time Password (OTP) has been dispatched to your email. Please
+          enter the OTP to continue.
         </p>
         {/* text end */}
       </div>
       {/* request-password-wrap end */}
+      {/* otp-wrap start */}
+      <div className="otp-field-wrap">
+        <ReactPinField
+          length={4}
+          className={`pin-field ${showActive && "pin-field-complete"}`}
+          onChange={(num: any) => {
+            setShowActive(false);
+            setError("");
+            setToken(num);
+          }}
+          onComplete={(num) => {
+            setShowActive(true);
+            // handleSubmitDirect(num);
+          }}
+          // disabled={completed}
+          validate="0123456789"
+          autoFocus
+          // ref={ref}
+        />
+      </div>
+      {/* otp-wrap end */}
 
       {/* revvex button start */}
       <RevvexButton
